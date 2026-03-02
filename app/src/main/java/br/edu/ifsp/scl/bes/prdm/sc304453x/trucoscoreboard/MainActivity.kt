3fb4149ddbeebdec8df4d378dcce2bd4786654b2
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-03-02 10:54:22 -0300

    feat: add methods to update the score

--- app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/trucoscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/trucoscoreboard/MainActivity.kt
@@ -1,20 +1,40 @@
 package br.edu.ifsp.scl.bes.prdm.sc304453x.trucoscoreboard
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.bes.prdm.sc304453x.trucoscoreboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val target = 12
+
+    private var usScore = 0
+
+    private var roundValue = RoundValue.ONE
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.usPointBt.setOnClickListener {
+            if (usScore < target) {
+                usScore = addPoints(usScore)
+                updateScore(activityMainBinding.usPointsTv, usScore)
+            }
+        } }
+
+    private fun addPoints(currentScore: Int): Int {
+        return currentScore + roundValue.points
+    }
+    private fun updateScore(textView: TextView, score: Int) {
+        textView.text = score.toString()
     }
 }
