7ed4705b3f682d50a1fe2ee47b6be4744f71f8a0
Fernando De Riggi <Fernando_De_Riggi@outlook.com>
2026-03-03 11:17:25 -0300

    refactor: add class Game

--- app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/trucoscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/bes/prdm/sc304453x/trucoscoreboard/MainActivity.kt
@@ -1,7 +1,6 @@
 package br.edu.ifsp.scl.bes.prdm.sc304453x.trucoscoreboard
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.bes.prdm.sc304453x.trucoscoreboard.databinding.ActivityMainBinding
@@ -12,69 +11,59 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
-    private val target = 12
-
-    private var usScore = 0
-    private var themScore = 0
-
-    private var roundValue = RoundValue.ONE
-    private var gameState = GameState.NORMAL
+    private val game = Game()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
 
-        updateRoundValue()
+        updateUI()
 
         activityMainBinding.usPointBt.setOnClickListener {
-            if (gameFinished()) return@setOnClickListener
 
-            usScore = addPoints(usScore)
-            updateScore(activityMainBinding.usPointsTv, usScore)
+            if (game.state == GameState.HAND_OF_ELEVEN) {
+                showHandOfElevenDialog()
+                return@setOnClickListener
+            }
 
-            checkWinner()
-            checkHandOfEleven()
+            game.addPoint(Team.US)
+            updateUI()
         }
 
         activityMainBinding.themPointBt.setOnClickListener {
-            if (gameFinished()) return@setOnClickListener
 
-            themScore = addPoints(themScore)
-            updateScore(activityMainBinding.themPointsTv, themScore)
+            if (game.state == GameState.HAND_OF_ELEVEN) {
+                showHandOfElevenDialog()
+                return@setOnClickListener
+            }
 
-            checkWinner()
-            checkHandOfEleven()
+            game.addPoint(Team.THEM)
+            updateUI()
         }
 
         activityMainBinding.trucoBt.setOnClickListener {
-            if (gameFinished()) return@setOnClickListener
-
-            roundValue = roundValue.next()
-            updateRoundValue()
+            game.callTruco()
+            updateUI()
         }
     }
 
-    private fun addPoints(currentScore: Int): Int {
-        val newScore = currentScore + roundValue.points
-        roundValue = RoundValue.ONE
-        updateRoundValue()
-        return newScore
-    }
+    private fun updateUI() {
+        activityMainBinding.usPointsTv.text = game.usScore.toString()
+        activityMainBinding.themPointsTv.text = game.themScore.toString()
+        activityMainBinding.usPointBt.text = "+${game.roundValue.points}"
+        activityMainBinding.themPointBt.text = "+${game.roundValue.points}"
 
-    private fun updateScore(textView: TextView, score: Int) {
-        textView.text = score.toString()
-    }
+        activityMainBinding.roundValueTv.text =
+            "Valendo: ${game.roundValue.points} pontos"
 
-    private fun updateRoundValue() {
-        activityMainBinding.roundValueTv.text = buildString {
-            append("Valendo: ")
-            append(roundValue.points)
-            append(" pontos")
+        if (game.state == GameState.FINISHED) {
+            disableGameButtons()
         }
-    }
 
-    private fun gameFinished(): Boolean {
-        return usScore >= target || themScore >= target
+        if (game.state == GameState.HAND_OF_ELEVEN) {
+            showHandOfElevenDialog()
+            activityMainBinding.trucoBt.isEnabled = false
+        }
     }
 
     private fun disableGameButtons() {
@@ -83,66 +72,19 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding.trucoBt.isEnabled = false
     }
 
-    private fun checkWinner() {
-        when {
-            usScore >= target -> {
-                activityMainBinding.roundValueTv.text = "Nós vencemos!"
-                disableGameButtons()
-            }
-            themScore >= target -> {
-                activityMainBinding.roundValueTv.text = "Eles venceram!"
-                disableGameButtons()
-            }
-        }
-    }
-
-    private fun checkHandOfEleven() {
-        if (gameFinished()) return
-
-        if (usScore == 11 && themScore < 11) {
-            gameState = GameState.HAND_OF_ELEVEN
-            showHandOfElevenDialog(isUs = true)
-        }
-
-        if (themScore == 11 && usScore < 11) {
-            gameState = GameState.HAND_OF_ELEVEN
-            showHandOfElevenDialog(isUs = false)
-        }
-    }
-
-    private fun showHandOfElevenDialog(isUs: Boolean) {
+    private fun showHandOfElevenDialog() {
         AlertDialog.Builder(this)
             .setTitle("Mão de 11")
             .setMessage("Deseja jogar ou correr?")
             .setCancelable(false)
             .setPositiveButton("Jogar") { _, _ ->
-                resolveHandOfEleven(play = true)
+                game.resolveHandOfEleven(true)
+                updateUI()
             }
             .setNegativeButton("Correr") { _, _ ->
-                resolveHandOfEleven(play = false)
+                game.resolveHandOfEleven(false)
+                updateUI()
             }
             .show()
-    }
-
-    private fun resolveHandOfEleven(play: Boolean) {
-        if (gameState != GameState.HAND_OF_ELEVEN) return
-
-        if (play) {
-            roundValue = RoundValue.THREE
-            updateRoundValue()
-        } else {
-            if (usScore == 11) {
-                themScore += 1
-                updateScore(activityMainBinding.themPointsTv, themScore)
-            } else {
-                usScore += 1
-                updateScore(activityMainBinding.usPointsTv, usScore)
-            }
-
-            checkWinner()
-            checkHandOfEleven()
-        }
-
-        gameState = GameState.HAND_OF_ELEVEN
     }
 }
